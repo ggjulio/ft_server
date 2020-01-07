5949c4254659d792669bddf226cf5e91a74ac68b
ggjulio <juligonz@student.42.fr>
2020-01-07 16:28:55 +0100

    .

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,14 @@ FROM debian:buster
 MAINTAINER juligonz@student.42.fr
 
 RUN apt-get update -qy \
-&& apt-get update -qy \
-&& apt-get install -qy nginx default-mysql-server php7.3-fpm php7.3-mysql \
-&&
+&& apt-get upgrade -qy \
+&& apt-get install -qy nginx default-mysql-server php7.3-fpm php7.3-mysql wget tar # tar ???
 
 #COPY ./srcs/* /usr/local/nginx/html/
+COPY ./srcs/* srcs_docker/
 
 # WORKDIR /usr/local/nginx/html/
 
-# RUN ls
-
 EXPOSE 80
 
-ENTRYPOINT ["docker-entrypoint.sh"]
-
+ENTRYPOINT ["srcs_docker/docker-entrypoint.sh"]
--- srcs/docker-entrypoint.sh
+++ srcs/docker-entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+
 service nginx start 
 service mysql start 
-service php7.3 start 
+service php7.3-fpm start 
+
+#########  install wordpress
+
+wget -c https://wordpress.org/latest.tar.gz -O - | tar -xz -C /var/www
+mv latest /etc/nginx/sites-available/wordpress
+#chown -R www-data: /var/www/wordpress      #allow webserver complete access
+
+#######  install phpmyadmin
+
+wget -c https://files.phpmyadmin.net/phpMyAdmin/4.9.2/phpMyAdmin-4.9.2-english.tar.gz -O - | tar -xz -C /var/www/phpmyadmin
+#mv phpMyAdmin-4.9.2-english /var/www/phpmyadmin
+
+service php7.3-fpm restart
+
+####### configure nginx for wordpress and phpmyadmin
+
+cp srcs_docker/nginx-block /etc/nginx/sites-available
+
+ln -s /etc/nginx/sites-available/wordpress  /etc/nginx/sites-enabled/wordpress
+#ln -s /etc/nginx/sites-available/phpmyadmin /etc/nginx/sites-enabled/phpmyadmin
+
+/bin/sh
